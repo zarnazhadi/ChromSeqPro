a97a5f015cf8914a5c587d4cee920a38387dd08b
zxh755 <66363888+zxh755@users.noreply.github.com>
2022-08-12 17:49:43 +0100

    Create 4-alignment.sh

---
+++ bash/4-alignment.sh
@@ -0,0 +1,29 @@
+# OPTIONS FOR GRID ENGINE ==============================================================
+#
+#$ -l h_rt=10:00:00
+#$ -l h_vmem=16G
+#$ -pe smp 8
+#$ -cwd
+#$ -j y
+#$ -V
+# OPTIONS FOR GRID ENGINE=================================================================
+
+export TRIMMED_DIR="/nobackup/bs21zh/ZH_prelim/bash/trimmed"
+export ALIGNED_DIR="/nobackup/bs21zh/ZH_prelim/bash/aligned"
+
+mkdir -p ${ALIGNED_DIR}
+
+cd ../index
+
+for r1 in ${TRIMMED_DIR}/*R1.fastq.gz
+do
+	r2=${r1/R1/R2}
+	fileid=$(basename $r1 _R1.fastq.gz) # getting file name only
+	
+	out=$( echo ${ALIGNED_DIR}/${fileid}.bam) # new file name after alignment
+	echo $out
+  
+  # BWA
+	/nobackup/leedsomics_tools/bwa-0.7.17/bwa mem -P -M -t 8 GRCh38.p13.genome.fa.gz $r1 $r2 > $out
+
+done
